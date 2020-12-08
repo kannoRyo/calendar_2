d8ea42813c3c9467317bd10c28106d1504dc268f
kanno <kannoryo@icloud.com>
2020-12-08 16:12:05 +0900

    基本ファイル作成

--- src/Calendar.js
+++ src/Calendar.js
@@ -3,7 +3,8 @@ import React from 'react'
 function App() {
   return (
     <div className="App">
-      React
+      <Nav />
+      <CalendarBoard />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Calendar from './Calendar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Calendar />
   </React.StrictMode>,
   document.getElementById('root')
 );
