09c1c09a7fc4f075f0a0664e423f85ea3ef7f96c
kanno <kannoryo@icloud.com>
2020-12-10 21:29:48 +0900

    カレンダーボードフォーマット作成

--- src/CalendarBoard.js
+++ src/CalendarBoard.js
@@ -15,87 +15,61 @@ const useStyles = makeStyles((theme) => ({
       width: '100%',
       height: 450,
     },
+    tile: {
+        height: 'auto',
+        width: '14%',
+        border: '1px solid #dbdbdb',
+        textAlign: 'center',
+        fontSize:'12px',
+        color:'#000',
+        padding: '6px 0'
+    }
   }));
 
 const CalendarBoard = ({board,setYear,setMonth,year,month})=>{
     const classes = useStyles() 
 
+    
 
-    console.log(renderDates(year,month))
-
+    const rows = [
+        renderDates(year,month).splice(0,7),
+        renderDates(year,month).splice(7,7),
+        renderDates(year,month).splice(14,7),
+        renderDates(year,month).splice(21,7),
+        renderDates(year,month).splice(28,7),
+    ]
 
     return(
             <GridList className={classes.gridList, board} style={{display:'flex'}} col={7}>
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border:'1px solid #dbdbdb'}}>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                
-                <GridListTile   style={{ height: 'auto', width: '14.2857%' , border: '1px solid #dbdbdb'} }>
-                </GridListTile>                           
+                {
+                    rows.map((dates,index)=>{
+                        return(
+                            <>
+                                <GridListTile class={classes.tile}>
+                                    <div>{dates[0]}</div>
+                                </GridListTile>  
+                                <GridListTile class={classes.tile}>
+                                    <div>{dates[1]}</div>
+                                </GridListTile>  
+                                <GridListTile class={classes.tile}>
+                                    <div>{dates[2]}</div>
+                                </GridListTile>  
+                                <GridListTile class={classes.tile}>
+                                    <div>{dates[3]}</div>
+                                </GridListTile>  
+                                <GridListTile class={classes.tile}>
+                                    <div>{dates[4]}</div>
+                                </GridListTile>  
+                                <GridListTile class={classes.tile}>
+                                    <div>{dates[5]}</div>
+                                </GridListTile>  
+                                <GridListTile class={classes.tile}>
+                                    <div>{dates[6]}</div>
+                                </GridListTile>  
+                            </>
+                        )
+                    })
+                }                         
             </GridList>
     )
 }
