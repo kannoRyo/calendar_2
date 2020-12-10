899a0c8d40ba877e342beb038db2b9d06a4002fa
kanno <kannoryo@icloud.com>
2020-12-10 14:02:12 +0900

    カレンダーレイアウト完成（リファクタリング未）

--- src/CalendarBoard.js
+++ src/CalendarBoard.js
@@ -1,8 +1,97 @@
 import React from 'react'
+import { makeStyles, GridList , GridListTile} from '@material-ui/core';
+import tileData from './img/tileData'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      margin:'0',
+      width: '100%',
+      height: 450,
+    },
+  }));
 
 const CalendarBoard = ({board})=>{
+    const classes = useStyles()
     return(
-        <div className={board}>board</div>
+            <GridList className={classes.gridList, board} style={{display:'flex'}} col={7}>
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                
+                <GridListTile key="Subheader"  style={{ height: 'auto', width: '14.2857%' }}>
+                </GridListTile>                           
+            </GridList>
     )
 }
 
---
+++ src/img/tileData.js
@@ -0,0 +1,11 @@
+const tileData = [
+    'https//material-ui.com/static/images/grid-list/camera.jpg', 
+    'https://material-ui.com/static/images/grid-list/burgers.jpg',
+    'https://material-ui.com/static/images/grid-list/camera.jpg',
+    'https://material-ui.com/static/images/grid-list/morning.jpg',
+    'https://material-ui.com/static/images/grid-list/hats.jpg',
+    'https://material-ui.com/static/images/grid-list/honey.jpg',
+    'https://material-ui.com/static/images/grid-list/vegetables.jpg'        
+] 
+
+export default tileData
